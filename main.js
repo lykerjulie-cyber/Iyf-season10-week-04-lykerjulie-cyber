6416ded6af9711ead620bffa6a98d5b3ec430e6c
lykerjulie-cyber <lykerjulie@gmail.com>
2026-04-26 08:00:31 +0300

    Add main.js with variable declarations and examples

---
+++ main.js
@@ -0,0 +1,18 @@
+// Variable declarations practice
+let name = "Your Name"; 
+let age = 25;
+const birthYear = 1999;
+
+// typeof operator
+console.log(typeof name);    // string
+console.log(typeof age);     // number
+console.log(typeof true);    // boolean
+
+// let vs const
+let score = 100;
+score = 150;  // Works!
+console.log("Updated score:", score);
+
+const PI = 3.14159;
+// PI = 3;  // Error! const cannot be reassigned - uncomment to test
+console.log("PI value:", PI);
