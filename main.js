dcfe5e0945fe6c77a989663323a2d9b2e50b59f2
lykerjulie-cyber <lykerjulie@gmail.com>
2026-04-26 08:01:30 +0300

    Add variable practice exercises for data types
    Added variable practice exercises demonstrating different data types.

--- main.js
+++ main.js
@@ -16,3 +16,30 @@ console.log("Updated score:", score);
 const PI = 3.14159;
 // PI = 3;  // Error! const cannot be reassigned - uncomment to test
 console.log("PI value:", PI);
+// Exercise: Variable Practice
+console.log("--- Variable Practice Exercise ---");
+
+// 1. Your name (string)
+let myName = "Alex Kim"; 
+console.log("My name is:", myName);
+console.log("Type of myName:", typeof myName);
+
+// 2. Your age (number) 
+let myAge = 22;
+console.log("My age is:", myAge);
+console.log("Type of myAge:", typeof myAge);
+
+// 3. Whether you're a student (boolean)
+let isStudent = true;
+console.log("Am I a student?", isStudent);
+console.log("Type of isStudent:", typeof isStudent);
+
+// 4. Your favorite colors (array - preview for next lesson)
+let favoriteColors = ["blue", "black", "forest green"];
+console.log("My favorite colors are:", favoriteColors);
+console.log("Type of favoriteColors:", typeof favoriteColors); // Note: arrays are type "object"
+
+// 5. Today's date (use new Date())
+const todaysDate = new Date();
+console.log("Today's date is:", todaysDate);
+console.log("Type of todaysDate:", typeof todaysDate); // Also "object"
